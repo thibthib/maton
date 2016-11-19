e3723c29421e865cbdb99ca17589304a8b44f112
Thibaut Dutartre <t.dutartre@gmail.com>
2016-11-19 14:10:55 +0100

    Improve server logs

--- server/server.js
+++ server/server.js
@@ -4,15 +4,21 @@ const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const datastore = require('./datastore.js');
+const chalk = require('chalk');
+
+const getConsoleTimestamp = () => {
+	const now = new Date();
+	return `${now.getHours() < 10 ? '0' : '' }${now.getHours()}:${now.getMinutes() < 10 ? '0' : '' }${now.getMinutes()}`;
+};
 
 const mesuresIO = io.of(mesuresNamespace);
 mesuresIO.on('connection', socket => {
 	const { machineId } = socket.handshake.query;
-	console.log(`🛰 Probe connection : ${machineId}`);
+	console.log(`${chalk.dim(getConsoleTimestamp())} 🛰 ${machineId} ➡️ ${chalk.blue(' probe connection')}`);
 	
 	socket.on(mesureLoadEvent, data => {
-		datastore.insert(machineId, data).then(inserted => {
-			console.log(inserted);
+		datastore.insert(machineId, data).then(() => {
+			console.log(`${chalk.dim(getConsoleTimestamp())} 🛰 ${machineId} ➡️ ${chalk.green(' measure insert')}`);
 		});
 	});
 });
