6e5e12726b621f59b70622c1b483775274638c6f
Thibaut Dutartre <t.dutartre@gmail.com>
2016-11-19 14:44:50 +0100

    Merge dashboard module in global maton project for config sharing

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Chart from '../components/Chart.js';
+import Chart from './components/Chart.js';
+const { getURL, api } = require('../common/configuration.js');
 import 'isomorphic-fetch';
 
 export default class App extends React.Component {
 	static getInitialProps() {
-		return fetch('http://localhost:8888/load')
+		return fetch(getURL(api.load))
 		.then(response => response.json())
 		.then(data => ({ data }));
 	}
