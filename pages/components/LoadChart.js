77012c4f7562d69c938c9c94813147ea414bf627
Thibaut Dutartre <t.dutartre@gmail.com>
2016-11-20 15:08:45 +0100

    Display thresold

--- pages/components/LoadChart.js
+++ pages/components/LoadChart.js
@@ -9,11 +9,15 @@ const style = {
 	width: 900,
 	height: 500,
 	bottomAxisHeight: 20,
-	rightAxisWidth: 30
+	rightAxisWidth: 30,
+	topPadding: 20
 };
 
-style.graphHeight = style.height-style.bottomAxisHeight;
+style.graphHeight = style.height-style.bottomAxisHeight-style.topPadding;
 style.graphWidth = style.width-style.rightAxisWidth;
+style.graphCSS = css({
+	paddingTop: `${style.topPadding}px`
+});
 style.bottomAxisCSS = css({
 	transform: `translateY(${style.graphHeight}px)`
 });
@@ -35,29 +39,33 @@ style.loadFifteenLineCSS = css({
 	stroke: '#FECD2F',
 	strokeWidth: 3
 });
+style.thresholdCSS = css({
+	stroke: '#EE605E',
+	strokeWidth: 1,
+	strokeDasharray: 3
+});
 
-const getXScale = (width) => {
+const getStateFromProps = props => {
 	const now = new Date();
 	const fifteenMinutesAgo = new Date(now);
 	fifteenMinutesAgo.setMinutes(now.getMinutes()-15);
 	
-	return d3.scaleTime()
-	.range([0, width])
-	.domain([fifteenMinutesAgo, now]);
-};
-
-const getYScale = (height, data) => {
-	return d3.scaleLinear()
-	.range([height, 0])
-	.domain([0, d3.max(data, d => d.load)]);
-};
-
-const getStateFromProps = props => {
+	const maxValue = Math.max(
+		d3.max(props.loadOne, d => d.load),
+		d3.max(props.loadFive, d => d.load),
+		d3.max(props.loadFifteen, d => d.load),
+		1
+	);
+	
 	return {
 		scales: {
-			x: getXScale(style.graphWidth),
-			y: getYScale(style.graphHeight, props.loadOne)
-		}
+			x: d3.scaleTime().range([0, style.graphWidth]).domain([fifteenMinutesAgo, now]),
+			y: d3.scaleLinear().range([style.graphHeight, 0]).domain([0, maxValue])
+		},
+		thresholdData: [
+			{ load: 1, timestamp: fifteenMinutesAgo },
+			{ load: 1, timestamp: now }
+		]
 	};
 };
 
@@ -79,12 +87,13 @@ export default class LoadChart extends React.Component {
 	}
 	render() {
 		return (
-			<svg width={style.width} height={style.height}>
+			<svg width={style.width} height={style.height} className={style.graphCSS}>
 				<g>
 					<Area data={this.props.loadOne} accessors={this.accessors} height={style.graphHeight} className={style.loadOneAreaCSS} />
 					<Line data={this.props.loadOne} accessors={this.accessors} className={style.loadOneLineCSS} />
 					<Line data={this.props.loadFive} accessors={this.accessors} className={style.loadFiveLineCSS} />
 					<Line data={this.props.loadFifteen} accessors={this.accessors} className={style.loadFifteenLineCSS} />
+					<Line data={this.state.thresholdData} accessors={this.accessors} className={style.thresholdCSS} />
 				</g>
 				<Axis generator={d3.axisBottom(this.state.scales.x)} className={style.bottomAxisCSS}/>
 				<Axis generator={d3.axisRight(this.state.scales.y)} className={style.rightAxisCSS}/>
