d12cf8044b1cc2044d324331f581bfbee26356fd
Thibaut Dutartre <thibaut.dutartre@weekendesk.fr>
2016-11-21 18:24:25 +0100

    Display alerts in the interface

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import LoadChart from './components/LoadChart.js';
 import 'isomorphic-fetch';
 import io from 'socket.io-client';
 import { getURL, sockets, api } from '../common/configuration.js';
+import css, { merge } from 'next/css';
+const { getConsoleTimestamp } = require('../common/log.js');
 
 const getLoadsFromMeasure = (measure, cores) => {
 	const timestamp = new Date(measure.timestamp);
@@ -13,47 +15,102 @@ const getLoadsFromMeasure = (measure, cores) => {
 	};
 };
 
+const getLoadArrays = (measures, cores, arrays = [[], [], []]) => {
+	return measures.reduce((arrays, measure) => {
+		const loads = getLoadsFromMeasure(measure, cores);
+		arrays[0].push(loads.loadOne);
+		arrays[1].push(loads.loadFive);
+		arrays[2].push(loads.loadFifteen);
+		return arrays;
+	}, arrays);
+};
+
+const style = {
+	chart: css({
+		display: 'inline-block'
+	}),
+	alerts: css({
+		display: 'inline-block',
+		width: '300px',
+		verticalAlign: 'top',
+		padding: '16px'
+	}),
+	alert: css({
+		padding: '4px 8px',
+		border: '1px solid black',
+		borderRadius: '4px',
+		marginBottom: '4px'
+	})
+};
+style.alertStart = merge(style.alert, css({
+	borderColor: '#EE605E',
+	backgroundColor: '#F09393',
+	color: '#A32828'
+}));
+style.alertEnd = merge(style.alert, css({
+	borderColor: '#99CCE6',
+	backgroundColor: '#C4E4F4',
+	color: '#266687'
+}));
+
 export default class Dashboard extends React.Component {
 	static getInitialProps() {
 		return fetch(getURL(api.load))
 		.then(response => response.json())
-		.then(data => ({ machine: data.machine, data: data.measures }));
+		.then(data => ({ machine: data.machine, measures: data.measures, alerts: data.alerts }));
 	}
 	constructor(props) {
 		super(props);
 		
-		this.state = props.data.reduce((state, measure) => {
-			const loads = getLoadsFromMeasure(measure, this.props.machine.cores);
-			state.loadOne.push(loads.loadOne);
-			state.loadFive.push(loads.loadFive);
-			state.loadFifteen.push(loads.loadFifteen);
-			return state;
-		}, {
-			loadOne: [],
-			loadFive: [],
-			loadFifteen: []
-		});
+		const loadArrays = getLoadArrays(props.measures, props.machine.cores);
+		
+		this.state = {
+			loadOne: loadArrays[0],
+			loadFive: loadArrays[1],
+			loadFifteen: loadArrays[2],
+			alerts: props.alerts
+		};
 	}
 	componentDidMount() {
 		const socket = io.connect(getURL(sockets.dashboard));
 		socket.on('new.measure', data => {
-			const loads = getLoadsFromMeasure(data, this.props.machine.cores);
-			const loadMeasures = {
-				loadOne: this.state.loadOne.concat(loads.loadOne),
-				loadFive: this.state.loadFive.concat(loads.loadFive),
-				loadFifteen: this.state.loadFifteen.concat(loads.loadFifteen)
-			};
-			this.setState(loadMeasures);
+			const newLoadArrays = getLoadArrays([data], this.props.machine.cores, [
+				this.state.loadOne.slice(0),
+				this.state.loadFive.slice(0),
+				this.state.loadFifteen.slice(0)
+			]);
+			this.setState({
+				loadOne: newLoadArrays[0],
+				loadFive: newLoadArrays[1],
+				loadFifteen: newLoadArrays[2]
+			});
+		});
+		socket.on('alert.start', alert => {
+			const newAlertArray = this.state.alerts.concat(Object.assign(alert, { type: 'start' }));
+			this.setState({ alerts: newAlertArray });
+		});
+		socket.on('alert.end', alert => {
+			const newAlertArray = this.state.alerts.concat(Object.assign(alert, { type: 'end' }));
+			this.setState({ alerts: newAlertArray });
 		});
 	}
 	render() {
 		return (
 			<div>
-				<LoadChart
-					loadOne={this.state.loadOne}
-					loadFive={this.state.loadFive}
-					loadFifteen={this.state.loadFifteen}
-				/>
+				<div className={style.chart}>
+					<LoadChart
+						loadOne={this.state.loadOne}
+						loadFive={this.state.loadFive}
+						loadFifteen={this.state.loadFifteen}
+					/>
+				</div>
+				<div className={style.alerts}>
+					{this.state.alerts.map((alert, index) => 
+						<div className={alert.type === 'start' ? style.alertStart : style.alertEnd} key={index}>
+							{`${getConsoleTimestamp(alert.timestamp)} ${alert.message}`}
+						</div>
+					)}
+				</div>
 			</div>
 		);
 	}
